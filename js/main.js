3561bc3ef6a6462312d57688ca35ea51d8afa77c
camilo13152 <caisazafo@unal.edu.co>
2017-04-11 23:21:27 -0500

    test

--- js/main.js
+++ js/main.js
@@ -10,7 +10,7 @@ $(document).ready(function() {
 
 
 var xmlhttp = new XMLHttpRequest();
-var url = "http://api.openweathermap.org/data/2.5/weather?q=chicago&appid=1d4ebab0fee09d9c5a41d255a004c7f5";
+var url = "http://api.darksky.net/forecast/915927622053e6e93854ede8d5d3360a/41.8708,-87.6505";
 xmlhttp.open("GET", url, true);
 xmlhttp.send();
 xmlhttp.onreadystatechange = function() {
@@ -19,9 +19,9 @@ xmlhttp.onreadystatechange = function() {
         var text = myArr;
         var json = JSON.parse(text);
         //alert(JSON.parse(text).coord.lon);    
-        document.getElementById("weather").innerHTML = "<center>Today the weather is <em>" + json.weather[0].main + "<img src=\"http://openweathermap.org/img/w/"+json.weather[0].icon+".png\"</em></center>";
+        document.getElementById("weather").innerHTML = "<center>Today the weather is <em>" + json.currently + "<img src=\"http://openweathermap.org/img/w/"+json.weather[0].icon+".png\"</em></center>";
         } else {
-        	document.getElementById("weather").innerHTML = "Actually we can't get the weather from Chicago :(";
+        	document.getElementById("weather").innerHTML = "Actually we can't get the weather from Chicago :("+ + json.currently ;
         }
     };
 
